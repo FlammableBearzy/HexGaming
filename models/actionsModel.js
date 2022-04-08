6cd482e53d8bd52621f30ccbe9f3e7d39d91f691
FlammableBearzy <50037276+FlammableBearzy@users.noreply.github.com>
2022-04-08 08:20:04 +0100

    Action Model update

--- models/actionsModel.js
+++ models/actionsModel.js
@@ -1,8 +1,8 @@
 var pool = require('./connection.js')
 
-module.export.getAllActions = async function() {
+module.export.getAllMoveActions = async function() {
     try {
-        let sql = `Select * from actions`;
+        let sql = `Select * from moveActions`;
         let result = await pool.query(sql);
         let cards = result.rows;
 
@@ -12,3 +12,16 @@ module.export.getAllActions = async function() {
         return { status: 500, result: err};
     }
 }
+
+module.export.getAllAttackActions = async function(){
+    try {
+        let sql = `Select * from attackAction`;
+        let result = await pool.query(sql);
+        let cards = result.rows;
+
+        return{ status: 200, result: cards};
+    } catch (err) {
+        console.log(err);
+        return { status: 500, result: err};
+    }
+}  
