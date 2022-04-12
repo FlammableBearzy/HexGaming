d332c519dfce489fbd523831ed455a631eeabc33
miguel81655 <58427064+miguel81655@users.noreply.github.com>
2022-04-12 19:24:12 +0100

    trap placer and new classes folder

--- public/sketch.js
+++ public/sketch.js
@@ -1,6 +1,6 @@
 //Board Related
 let boardClass;
-let newBoard = [,];
+let newBoard = [];
 
 //Trap
 let traps = [];
@@ -9,11 +9,16 @@ function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
   boardClass = new board();
 
-  newBoard = boardClass.createBoard(5,3,150);
-  traps.push(new boardTrap(newBoard[3],1,1))
-  traps[0].placeTrap();
+  newBoard = boardClass.createBoard(6,3,150);
+  traps.push(new boardTrap(newBoard[7],1,1))
+  traps.push(new boardTrap(newBoard[8],1,1))
+  traps.push(new boardTrap(newBoard[15],1,1))
+  traps.push(new boardTrap(newBoard[12],1,1))
+  
 }
 
 function draw() {
-    
+  for (let i = 0; i < traps.length;i++){
+    traps[i].placeTrap();
+  }
 }
