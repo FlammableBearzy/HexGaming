f17fe3195dd0711b77d8ead1582d67a07554545d
FlammableBearzy <50037276+FlammableBearzy@users.noreply.github.com>
2022-06-15 12:15:55 +0100

    Update attack.js

--- public/classes/attack.js
+++ public/classes/attack.js
@@ -9,14 +9,17 @@ class attackCreator{
 
     attackplacer(newParcel)
     {
+        console.log("Before if in attackplacer");
         if(newParcel != null)
         {
+            console.log("Inside if in attackplacer");
             this.parcel = newParcel;
             fill(this.color)
             circle(this.parcel.centerx, this.parcel.centery, this.size)
         }
         else
         {
+            console.log("Inside else in attackplacer");
             fill(this.color)
             circle(this.parcel.centerx, this.parcel.centery, this.size)
         }
